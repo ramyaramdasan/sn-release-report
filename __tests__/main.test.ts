80417cd0c08061beb694abbd47605b2f2f7346cf
ramyaramdasan <62088522+ramyaramdasan@users.noreply.github.com>
2021-06-28 10:51:52 +0530

    updates

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -5,7 +5,8 @@ jest.mock('@actions/core')
 describe('when running the action',()=>{
 const fakeSetOutput=core.setOutput as jest.MockedFunction<typeof core.setOutput>
 test('it should set the release-url as output parameter', async()=>{
- await run()
+ await run(){
  expect(fakeSetOutput).toHaveBeenCalledWith('release-url', expect.anything()) 
+ }
 })
 })
